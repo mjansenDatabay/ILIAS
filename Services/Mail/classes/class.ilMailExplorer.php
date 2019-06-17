0e212fb3c15685f1f3e36cbd43d3f328c1ea39de
Niels Theen <ntheen@databay.de>
2019-06-17 11:11:42 +0200

    Remove unused  prefix from variable names

--- Services/Mail/classes/class.ilMailExplorer.php
+++ Services/Mail/classes/class.ilMailExplorer.php
@@ -58,44 +58,44 @@ class ilMailExplorer implements \ILIAS\UI\Component\Tree\TreeRecursion
 
     /**
      * ilMailExplorer constructor.
-     * @param $a_parent_obj
-     * @param $a_parent_cmd
-     * @param $a_user_id
+     * @param $parentObject
+     * @param $parenteCommand
+     * @param $userId
      */
-    public function __construct($a_parent_obj, $a_parent_cmd, $a_user_id)
+    public function __construct($parentObject, $parenteCommand, $userId)
     {
         global $DIC;
 
         $this->lng          = $DIC->language();
         $this->ctrl         = $DIC->ctrl();
         $this->httpRequest  = $DIC->http()->request();
-        $this->parentObject = $a_parent_obj;
+        $this->parentObject = $parentObject;
         $this->ui           = $DIC->ui();
 
-        $this->tree = new ilTree($a_user_id);
+        $this->tree = new ilTree($userId);
         $this->tree->setTableNames('mail_tree', 'mail_obj_data');
     }
 
-    function getNodeContent($a_node)
+    function getNodeContent($node)
     {
-        $content = $a_node['title'];
+        $content = $node['title'];
 
-        if ($a_node['child'] == $this->getNodeId($this->getRootNode())) {
+        if ($node['child'] == $this->getNodeId($this->getRootNode())) {
             $content = $this->lng->txt('mail_folders');
-        } elseif ($a_node['depth'] < 3) {
-            $content = $this->lng->txt('mail_' . $a_node['title']);
+        } elseif ($node['depth'] < 3) {
+            $content = $this->lng->txt('mail_' . $node['title']);
         }
 
         return $content;
     }
 
-    function getNodeIcon($a_node)
+    function getNodeIcon($node)
     {
-        if ($a_node['child'] == $this->getNodeId($this->getRootNode())) {
+        if ($node['child'] == $this->getNodeId($this->getRootNode())) {
             $icon = ilUtil::getImagePath('icon_mail.svg');
         } else {
-            $iconType = $a_node['m_type'];
-            if ($a_node['m_type'] === 'user_folder') {
+            $iconType = $node['m_type'];
+            if ($node['m_type'] === 'user_folder') {
                 $iconType = 'local';
             }
 
@@ -105,37 +105,37 @@ class ilMailExplorer implements \ILIAS\UI\Component\Tree\TreeRecursion
         return $icon;
     }
 
-    function getNodeIconAlt($a_node)
+    function getNodeIconAlt($node)
     {
-        $text = $this->lng->txt('icon') . ' ' . $this->lng->txt($a_node['m_type']);
+        $text = $this->lng->txt('icon') . ' ' . $this->lng->txt($node['m_type']);
 
-        if ($a_node['child'] == $this->getNodeId($this->getRootNode())) {
+        if ($node['child'] == $this->getNodeId($this->getRootNode())) {
             $text = $this->lng->txt('icon') . ' ' . $this->lng->txt('mail_folders');
         }
 
         return $text;
     }
 
-    function getNodeHref($a_node)
+    function getNodeHref($node)
     {
-        if ($a_node['child'] == $this->getNodeId($this->getRootNode())) {
-            $a_node['child'] = 0;
+        if ($node['child'] == $this->getNodeId($this->getRootNode())) {
+            $node['child'] = 0;
         }
 
-        $this->ctrl->setParameterByClass('ilMailFolderGUI', 'mobj_id', $a_node['child']);
+        $this->ctrl->setParameterByClass('ilMailFolderGUI', 'mobj_id', $node['child']);
         $href = $this->ctrl->getLinkTargetByClass('ilMailFolderGUI', '', '', false, false);
         $this->ctrl->clearParametersByClass('ilMailFolderGUI');
 
         return $href;
     }
 
-    function isNodeHighlighted($a_node)
+    function isNodeHighlighted($node)
     {
         $folderId = (int) ($this->httpRequest->getQueryParams()['mobj_id'] ?? 0);
 
         if (
-            $a_node['child'] == $folderId ||
-            (0 === $folderId && $a_node['child'] == $this->getNodeId($this->getRootNode()))
+            $node['child'] == $folderId ||
+            (0 === $folderId && $node['child'] == $this->getNodeId($this->getRootNode()))
         ) {
             return true;
         }
@@ -271,25 +271,24 @@ class ilMailExplorer implements \ILIAS\UI\Component\Tree\TreeRecursion
 
     /**
      * Get childs of node
-     *
-     * @param int $a_parent_node_id parent id
+     * @param int $parentNodeId parent id
      * @return array childs
      */
-    private function getChildsOfNode($a_parent_node_id)
+    private function getChildsOfNode($parentNodeId)
     {
         if ($this->preloaded && $this->getSearchTerm() == "") {
-            if (is_array($this->childs[$a_parent_node_id])) {
-                return $this->childs[$a_parent_node_id];
+            if (is_array($this->childs[$parentNodeId])) {
+                return $this->childs[$parentNodeId];
             }
             return array();
         }
 
         $whitelist = $this->getTypeWhiteList();
         if (is_array($whitelist) && count($whitelist) > 0) {
-            $childs = $this->tree->getChildsByTypeFilter($a_parent_node_id, $whitelist, $this->getOrderField());
+            $childs = $this->tree->getChildsByTypeFilter($parentNodeId, $whitelist, $this->getOrderField());
         }
         else {
-            $childs = $this->tree->getChilds($a_parent_node_id, $this->getOrderField());
+            $childs = $this->tree->getChilds($parentNodeId, $this->getOrderField());
         }
 
         // apply black list filter
@@ -407,13 +406,12 @@ class ilMailExplorer implements \ILIAS\UI\Component\Tree\TreeRecursion
 
     /**
      * Get id for node
-     *
-     * @param mixed $a_node node object/array
+     * @param mixed $node node object/array
      * @return string id
      */
-    private function getNodeId($a_node)
+    private function getNodeId($node)
     {
-        return $a_node["child"];
+        return $node["child"];
     }
 
     /**
