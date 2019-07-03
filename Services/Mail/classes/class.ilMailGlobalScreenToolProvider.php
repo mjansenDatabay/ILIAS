09934f03e611a2c71ef0d5f35041a57c8b94fd1c
Niels Theen <ntheen@databay.de>
2019-07-03 11:42:01 +0200

    Code Style fix

--- Services/Mail/classes/class.ilMailGlobalScreenToolProvider.php
+++ Services/Mail/classes/class.ilMailGlobalScreenToolProvider.php
@@ -9,35 +9,36 @@ use ILIAS\GlobalScreen\Scope\Tool\Provider\AbstractDynamicToolProvider;
 class ilMailGlobalScreenToolProvider extends AbstractDynamicToolProvider
 {
     const SHOW_MAIL_FOLDERS_TOOL = 'show_mail_folders_tool';
-	/**
-	 * @inheritDoc
-	 */
-	public function isInterestedInContexts(): \ILIAS\GlobalScreen\Scope\Tool\Context\Stack\ContextCollection
-	{
+
+    /**
+     * @inheritDoc
+     */
+    public function isInterestedInContexts() : \ILIAS\GlobalScreen\Scope\Tool\Context\Stack\ContextCollection
+    {
         return $this->context_collection->main()->repository()->administration();
-	}
+    }
 
-	/**
-	 * @inheritDoc
-	 */
-	public function getToolsForContextStack(\ILIAS\GlobalScreen\Scope\Tool\Context\Stack\CalledContexts $called_contexts): array
-	{
-		$identification = function ($id) {
-		    return $this->identification_provider->identifier($id);
-		};
+    /**
+     * @inheritDoc
+     */
+    public function getToolsForContextStack(\ILIAS\GlobalScreen\Scope\Tool\Context\Stack\CalledContexts $called_contexts) : array
+    {
+        $identification = function ($id) {
+            return $this->identification_provider->identifier($id);
+        };
 
-		$tools = [];
+        $tools = [];
 
-		$baseClass = (string) ($this->dic->http()->request()->getQueryParams()['baseClass'] ?? '');
-		if (strtolower($baseClass) === 'ilmailgui') {
-			$exp = new ilMailExplorer(new ilMailGUI(), $this->dic->user()->getId());
+        $baseClass = (string) ($this->dic->http()->request()->getQueryParams()['baseClass'] ?? '');
+        if (strtolower($baseClass) === 'ilmailgui') {
+            $exp = new ilMailExplorer(new ilMailGUI(), $this->dic->user()->getId());
 
             $tools[] = $this->factory
                 ->tool($identification('tree'))
                 ->withTitle($this->dic->language()->txt("mail_folders"))
                 ->withContent($this->dic->ui()->factory()->legacy($exp->getHTML()));
-		}
+        }
 
-		return $tools;
-	}
+        return $tools;
+    }
 }
