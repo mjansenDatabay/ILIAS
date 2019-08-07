852544a263c32484d0bc71a2e55e414615c18eef
Niels Theen <ntheen@databay.de>
2019-08-07 11:41:02 +0200

    Fix copyright copy + pasted

--- src/UI/Component/Tree/Node/Bylined.php
+++ src/UI/Component/Tree/Node/Bylined.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-/* Copyright (c) 2019 Nils Haagen <nils.haagen@concepts-and-training.de> Extended GPL, see docs/LICENSE */
+/* Copyright (c) 1998-2019 ILIAS open source, Extended GPL, see docs/LICENSE */
 
 namespace ILIAS\UI\Component\Tree\Node;
 
