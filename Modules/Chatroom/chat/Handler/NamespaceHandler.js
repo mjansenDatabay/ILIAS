100298900e36b2d2bd44ac3717dd56ac68644232
mjansen <mjansen@databay.de>
2020-01-31 17:10:31 +0100

    ChatServer: Implemented message queue to reduce SQL communication and prevent memory leaks/throughput issues

--- Modules/Chatroom/chat/Handler/NamespaceHandler.js
+++ Modules/Chatroom/chat/Handler/NamespaceHandler.js
@@ -2,7 +2,7 @@ var Namespace = require('../Model/Namespace');
 var MessageQueue = require('../Model/MessageQueue');
 var Container = require('../AppContainer');
 
-module.exports = function createNamespace(name) {
+module.exports.createNamespace = function createNamespace(name) {
 	var namespace = new Namespace(name, new MessageQueue());
 
 	Container.addNamespace(namespace);
