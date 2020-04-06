276ac9b63c8b8ceb9ff5a4f55e22a702bd71cfd9
Fred Neumann <fred.neumann@fau.de>
2020-04-06 17:22:19 +0200

    fix saml account creation

--- Services/Saml/classes/class.ilAuthProviderSamlStudOn.php
+++ Services/Saml/classes/class.ilAuthProviderSamlStudOn.php
@@ -238,7 +238,7 @@ class ilAuthProviderSamlStudOn extends ilAuthProviderSaml
             $rawdata['given_name'] = $this->attributes['urn:mace:dir:attribute-def:givenName'][0];
             $rawdata['mail'] = $this->attributes['urn:mace:dir:attribute-def:mail'][0];
             $rawdata['schac_gender'] = $this->attributes['urn:mace:terena.org:attribute-def:schacGender'][0];
-            $rawdata['unscoped_affiliation'] = implode(';', $this->attributes['urn:mace:dir:attribute-def:eduPersonAffiliation']);
+            $rawdata['unscoped_affiliation'] = implode(';', (array) $this->attributes['urn:mace:dir:attribute-def:eduPersonAffiliation']);
             // Passwords by SSO have {CRYPT} prefix - not yet supported by StudOn
             //$rawdata['user_password']               = $this->attributes['urn:mace:dir:attribute-def:userPassword'][0];
             $rawdata['schac_personal_unique_code'] = $this->attributes['urn:mace:terena.org:attribute-def:schacPersonalUniqueCode'][0];
