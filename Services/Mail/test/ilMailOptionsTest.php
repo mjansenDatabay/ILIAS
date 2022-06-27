2c89602f2a9739d92dc7a7d9967df47592a0058c
Fabian Helfer <fhelfer@nb-fhelfer.office.databay.de>
2022-06-27 15:38:31 +0200

    unit tests mailoptions isAbsent

--- Services/Mail/test/ilMailOptionsTest.php
+++ Services/Mail/test/ilMailOptionsTest.php
@@ -16,6 +16,8 @@
  *
  *********************************************************************/
 
+use ILIAS\Data\Clock\ClockInterface;
+
 /**
  * Class ilMailOptionsTest
  * @author Niels Theen <ntheen@databay.de>
@@ -70,5 +72,85 @@ class ilMailOptionsTest extends ilMailBaseTest
         $this->assertSame($object->absent_until, $mailOptions->getAbsentUntil());
         $this->assertSame($object->absence_ar_subject, $mailOptions->getAbsenceAutoResponderSubject());
         $this->assertSame($object->absence_ar_body, $mailOptions->getAbsenceAutoResponderBody());
+    }
+
+    /**
+     * @dataProvider provideMailOptionsData
+     */
+    public function testIsAbsent($absence_status, $absent_from, $absent_until, $result) : void
+    {
+        $userId = 1;
+        $object = new stdClass();
+        $object->cronjob_notification = false;
+        $object->signature = 'smth';
+        $object->linebreak = 0;
+        $object->incoming_type = 1;
+        $object->mail_address_option = 0;
+        $object->email = 'test@test.com';
+        $object->second_email = 'ilias@ilias.com';
+        $object->absence_status = $absence_status;
+        $object->absent_from = $absent_from;
+        $object->absent_until = $absent_until;
+        $object->absence_ar_subject = 'subject';
+        $object->absence_ar_body = 'body';
+        $database = $this->getMockBuilder(ilDBInterface::class)->getMock();
+        $queryMock = $this->getMockBuilder(ilDBStatement::class)->getMock();
+
+        $clockService = $this->getMockBuilder(ClockInterface::class)->disableOriginalConstructor()->getMock();
+        $clockService->method('now')->willReturn(new DateTimeImmutable('NOW'));
+
+        $database->expects($this->once())->method('fetchObject')->willReturn($object);
+        $database->expects($this->once())->method('queryF')->willReturn($queryMock);
+
+        $this->setGlobalVariable('ilDB', $database);
+
+        $settings = $this->getMockBuilder(ilSetting::class)->disableOriginalConstructor()->onlyMethods([
+            'set',
+            'get',
+        ])->getMock();
+        $this->setGlobalVariable('ilSetting', $settings);
+
+        $mailOptions = new ilMailOptions(1, null, $clockService);
+        $this->assertEquals($mailOptions->isAbsent(), $result);
+    }
+
+    public function provideMailOptionsData() : Generator
+    {
+
+        yield 'correct configuration' => [
+            'absence_status' => true,
+            'absent_from' => time(),
+            'absent_until' => time(),
+            'result' => true,
+        ];
+
+        yield 'not absent' => [
+            'absence_status' => false,
+            'absent_from' => time(),
+            'absent_until' => time(),
+            'result' => false,
+        ];
+
+        yield 'absent, absent_from is in the future' => [
+            'absence_status' => true,
+            'absent_from' => time() + 1,
+            'absent_until' => time(),
+            'result' => false,
+        ];
+
+        yield 'absent, absent_until is in the past' => [
+            'absence_status' => true,
+            'absent_from' => time(),
+            'absent_until' => time() - 1,
+            'result' => false,
+        ];
+
+        yield 'absent, absent_from is in the past, absent_until is in the future' => [
+            'absence_status' => true,
+            'absent_from' => time() - 1,
+            'absent_until' => time() + 1,
+            'result' => true,
+        ];
+
     }
 }
