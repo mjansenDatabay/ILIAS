cd46c5d9b691b9f91cab1a1750a8cfc1efe242ae
Fabian Helfer <fhelfer@nb-fhelfer.office.databay.de>
2022-07-18 15:45:11 +0200

    first draft of an auto responder with idle time

--- Services/Mail/classes/class.ilMailFormGUI.php
+++ Services/Mail/classes/class.ilMailFormGUI.php
@@ -195,7 +195,7 @@ class ilMailFormGUI
 
         $mailer->setSaveInSentbox(true);
 
-        $mailer = $mailer->enableAutoResponder();
+        $mailer->getAutoResponderService()->enableAutoResponder();
 
         if ($errors = $mailer->enqueue(
             ilUtil::securePlainString($this->getBodyParam('rcp_to', $this->refinery->kindlyTo()->string(), '')),
@@ -209,7 +209,7 @@ class ilMailFormGUI
             $this->requestAttachments = $files;
             $this->showSubmissionErrors($errors);
         } else {
-            $mailer = $mailer->disableAutoResponder();
+            $mailer->getAutoResponderService()->disableAutoResponder();
 
             $mailer->savePostData(
                 $this->user->getId(),
@@ -230,7 +230,7 @@ class ilMailFormGUI
                 $this->ctrl->redirectByClass(ilMailGUI::class);
             }
         }
-        $mailer = $mailer->disableAutoResponder();
+        $mailer->getAutoResponderService()->disableAutoResponder();
 
         $this->showForm();
     }
